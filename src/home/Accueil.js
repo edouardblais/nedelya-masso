100ad30125fb7f43dd30a85b49e7b08d175094f3
edouard <edouard.blais@hotmail.com>
2022-11-20 13:12:50 -0500

    pursued home structure

--- src/home/Accueil.js
+++ src/home/Accueil.js
@@ -6,25 +6,35 @@ const Accueil = () => {
     return (
         <div className='home-box'>
             <div className='overview-box'>
-                <div className='text-box'></div>
+                <div className='image-box'></div>
+                <div className='text-box'>
+                    <p className='home-text'>La massothérapie à son meilleur</p>
+                    <button className='home-btn'>RÉSERVEZ MAINTENANT</button>
+                </div>
+            </div>
+            <div className='overview-box'>
+                <div className='text-box'>
+                    <p className='home-text'>Peu importe votre bobo, Ned peut vous aider.</p>
+                    <button className='home-btn'>SERVICES</button>
+                </div>
                 <div className='image-box'></div>
             </div>
             <div className='overview-box'>
-                <div className='text-box'></div>
-                <div className='image-box'></div>
-            </div>
-            <div className='overview-box'>
+                <div className='image-box'>
+                    <img src={Nedso} alt='Ned' className='home-image'/>
+                </div>
                 <div className='text-box'>
                     <p className='home-text'>Ned est pro. Engage Ned.</p>
                     <button className='home-btn'>EN SAVOIR PLUS</button>
                 </div>
-                <div className='image-box'>
-                    <img src={Nedso} alt='picture of Ned' className='home-image'/>
-                </div>
             </div>
             <div className='overview-box'>
-                <div className='text-box'></div>
-                <div className='image-box'></div>
+                <div className='image-box-stretch-skala'>
+                    <div className='text-box-skala'>
+                        <p className='home-text'>Produits recommandés</p>
+                        <button className='home-btn'>BOUTIQUE</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/home/Home.js
+++ src/home/Home.js
@@ -6,25 +6,35 @@ const Home = () => {
     return (
         <div className='home-box'>
             <div className='overview-box'>
-                <div className='text-box'></div>
                 <div className='image-box'></div>
-            </div>
-            <div className='overview-box'>
-                <div className='text-box'></div>
-                <div className='image-box'></div>
+                <div className='text-box'>
+                    <p className='home-text'>Massotherapy like you've never experienced it before.</p>
+                    <button className='home-btn'>BOOK NOW</button>
+                </div>
             </div>
             <div className='overview-box'>
                 <div className='text-box'>
-                    <p className='home-text'>Ned is pro. Hire Ned.</p>
-                    <button className='home-btn'>See Bio</button>
+                    <p className='home-text'>Whatever your need, Ned can help you.</p>
+                    <button className='home-btn'>SERVICES</button>
                 </div>
+                <div className='image-box'></div>
+            </div>
+            <div className='overview-box'>
                 <div className='image-box'>
-                    <img src={Nedso} alt='picture of Ned' className='home-image'/>
+                    <img src={Nedso} alt='Ned' className='home-image'/>
+                </div>
+                <div className='text-box'>
+                    <p className='home-text'>Ned is pro. Hire Ned.</p>
+                    <button className='home-btn'>KNOW MORE</button>
                 </div>
             </div>
             <div className='overview-box'>
-                <div className='text-box'></div>
-                <div className='image-box'></div>
+                <div className='image-box-stretch-skala'>
+                    <div className='text-box-skala'>
+                        <p className='home-text'>Recommended products</p>
+                        <button className='home-btn'>SHOP</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
