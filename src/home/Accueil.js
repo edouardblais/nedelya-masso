189693016814d9de8edea79b96e192dc226e16e5
edouard <edouard.blais@hotmail.com>
2022-11-24 22:36:53 -0500

    continued home page setup

--- src/home/Accueil.js
+++ src/home/Accueil.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './home.css';
 import Nedso from '../img/nedso.jpg';
+import Nedmass from '../img/nedmass.jpg';
 
 const Accueil = () => {
     return (
         <div className='home-box'>
             <div className='overview-box'>
-                <div className='image-box'></div>
-                <div className='text-box'>
-                    <p className='home-text'>La massothérapie à son meilleur</p>
-                    <button className='home-btn'>RÉSERVEZ MAINTENANT</button>
+                <div className='image-box-stretch nedclimb'>
+                    <div className='text-box-nedclimb'>
+                        <p className='home-text'>La massothérapie pour les athlètes par une athlète</p>
+                        <button className='home-btn'>RÉSERVEZ MAINTENANT</button>
+                    </div>
                 </div>
             </div>
             <div className='overview-box'>
@@ -17,7 +19,17 @@ const Accueil = () => {
                     <p className='home-text'>Peu importe votre bobo, Ned peut vous aider.</p>
                     <button className='home-btn'>SERVICES</button>
                 </div>
-                <div className='image-box'></div>
+                <div className='image-box'>
+                    <img src={Nedmass} alt='massaging a client' className='home-image'/>
+                </div>
+            </div>
+            <div className='overview-box'>
+                <div className='image-box-stretch skala'>
+                    <div className='text-box-skala'>
+                        <p className='home-text'>Produits recommandés</p>
+                        <button className='home-btn'>BOUTIQUE</button>
+                    </div>
+                </div>
             </div>
             <div className='overview-box'>
                 <div className='image-box'>
@@ -26,14 +38,6 @@ const Accueil = () => {
                 <div className='text-box'>
                     <p className='home-text'>Ned est pro. Engage Ned.</p>
                     <button className='home-btn'>EN SAVOIR PLUS</button>
-                </div>
-            </div>
-            <div className='overview-box'>
-                <div className='image-box-stretch-skala'>
-                    <div className='text-box-skala'>
-                        <p className='home-text'>Produits recommandés</p>
-                        <button className='home-btn'>BOUTIQUE</button>
-                    </div>
                 </div>
             </div>
         </div>
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './home.css';
 import Nedso from '../img/nedso.jpg';
+import Nedmass from '../img/nedmass.jpg';
 
 const Home = () => {
     return (
         <div className='home-box'>
             <div className='overview-box'>
-                <div className='image-box'></div>
-                <div className='text-box'>
-                    <p className='home-text'>Massotherapy like you've never experienced it before.</p>
-                    <button className='home-btn'>BOOK NOW</button>
+                <div className='image-box-stretch nedclimb'>
+                    <div className='text-box-nedclimb'>
+                        <p className='home-text'>Massotherapy for athletes by an athlete</p>
+                        <button className='home-btn'>BOOK NOW</button>
+                    </div>
                 </div>
             </div>
             <div className='overview-box'>
@@ -17,7 +19,17 @@ const Home = () => {
                     <p className='home-text'>Whatever your need, Ned can help you.</p>
                     <button className='home-btn'>SERVICES</button>
                 </div>
-                <div className='image-box'></div>
+                <div className='image-box'>
+                    <img src={Nedmass} alt='massaging a client' className='home-image'/>
+                </div>
+            </div>
+            <div className='overview-box'>
+                <div className='image-box-stretch skala'>
+                    <div className='text-box-skala'>
+                        <p className='home-text'>Recommended products</p>
+                        <button className='home-btn'>SHOP</button>
+                    </div>
+                </div>
             </div>
             <div className='overview-box'>
                 <div className='image-box'>
@@ -26,14 +38,6 @@ const Home = () => {
                 <div className='text-box'>
                     <p className='home-text'>Ned is pro. Hire Ned.</p>
                     <button className='home-btn'>KNOW MORE</button>
-                </div>
-            </div>
-            <div className='overview-box'>
-                <div className='image-box-stretch-skala'>
-                    <div className='text-box-skala'>
-                        <p className='home-text'>Recommended products</p>
-                        <button className='home-btn'>SHOP</button>
-                    </div>
                 </div>
             </div>
         </div>
